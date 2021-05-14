647fd4fba586528890fe7d4a21f7e6d5cf514841
Julien Dendauw <dauwbot@pm.me>
2021-05-14 19:41:50 +0200

    Some more refactor

--- triplex.cpp
+++ triplex.cpp
@@ -6,10 +6,8 @@ bool GuessNumbers() {
 
 int main() {
     // General description
-    std::cout << "You're the dumbest person on earth and need to enter the correct code to collect your booze";
-    std::cout << std::endl;
-    std::cout << "Enter the correct code to access the tavern...";
-    std::cout << std::endl;
+    std::cout << "You're the dumbest person on earth and need to enter the correct code to collect your booze \n";
+    std::cout << "Enter the correct code to access the tavern... \n";
 
     int CodeA = 4;
     int CodeB = 3;
