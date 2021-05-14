577311bf85d2d25d931402daa6b5fb0c82145869
Julien Dendauw <dauwbot@pm.me>
2021-05-14 16:29:10 +0200

    Added variables

--- triplex.cpp
+++ triplex.cpp
@@ -3,6 +3,15 @@
 int main() {
     std::cout << "You're the dumbest person on earth and need to enter the correct code to collect your booze";
     std::cout << std::endl;
-    std::cout << "Enter the correct code to access the tavern...\n";
+    std::cout << "Enter the correct code to access the tavern...";
+    std::cout << std::endl;
+
+
+    int a = 4;
+    int b = 5;
+    int c = 7;
+
+    std::cout << a;
+
     return 0;
 }
