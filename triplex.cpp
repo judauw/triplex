b8b8fed285dc680c809515a5cd4594a60d93294a
Julien Dendauw <dauwbot@pm.me>
2021-05-14 19:40:58 +0200

    Code refactor

--- triplex.cpp
+++ triplex.cpp
@@ -11,8 +11,7 @@ int main() {
     std::cout << "Enter the correct code to access the tavern...";
     std::cout << std::endl;
 
-    /* Assigning variables */
-    const int CodeA = 4;
+    int CodeA = 4;
     int CodeB = 3;
     int CodeC = 2;
 
@@ -20,16 +19,12 @@ int main() {
     int CodeProd = CodeA * CodeB * CodeC;
 
     // Print sum and product
-    std::cout << "There are 3 numbers in the code" << std::endl;
-    std::cout << "The code adds up to: ";
-    std::cout << CodeSum << std::endl;
-    std::cout << "The code multiply to give: ";
-    std::cout << CodeProd << std::endl;
+    std::cout << "There are 3 numbers in the code";
+    std::cout << "\nThe code adds up to: " << CodeSum;
+    std::cout << "\nThe code multiply to give: " << CodeProd;
 
     int PlayerGuessA, PlayerGuessB, PlayerGuessC;
-    std::cin >> PlayerGuessA;
-    std::cin >> PlayerGuessB;
-    std::cin >> PlayerGuessC;
+    std::cin >> PlayerGuessA >> PlayerGuessB >> PlayerGuessC;
 
     int GuessSum = PlayerGuessA + PlayerGuessB + PlayerGuessC;
     int GuessProd = PlayerGuessA * PlayerGuessB * PlayerGuessC;
