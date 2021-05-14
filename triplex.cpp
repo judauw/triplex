4e9d672bfab699213b9467d6846a8680d625c4e3
Julien Dendauw <dauwbot@pm.me>
2021-05-14 19:10:58 +0200

    Added cin to our code

--- triplex.cpp
+++ triplex.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+bool GuessNumbers() {
+    return true;
+}
+
 int main() {
     // General description
     std::cout << "You're the dumbest person on earth and need to enter the correct code to collect your booze";
@@ -8,16 +12,25 @@ int main() {
     std::cout << std::endl;
 
     /* Assigning variables */
-    const int a = 4;
-    int b = 3;
-    int c = 2;
+    const int CodeA = 4;
+    int CodeB = 3;
+    int CodeC = 2;
 
-    int sum = a + b + c;
-    int prod = a * b * c;
+    int CodeSum = CodeA + CodeB + CodeC;
+    int CodeProd = CodeA * CodeB * CodeC;
 
     // Print sum and product
-    std::cout << sum << std::endl;
-    std::cout << prod;
+    std::cout << "There are 3 numbers in the code" << std::endl;
+    std::cout << "The code adds up to: ";
+    std::cout << CodeSum << std::endl;
+    std::cout << "The code multiply to give: ";
+    std::cout << CodeProd << std::endl;
+
+    int PlayerGuessA, PlayerGuessB, PlayerGuessC;
+    std::cin >> PlayerGuessA;
+    std::cin >> PlayerGuessB;
+    std::cin >> PlayerGuessC;
+    std::cout << "You entered: " << PlayerGuessA << PlayerGuessB << PlayerGuessC;
 
     return 0;
 }
