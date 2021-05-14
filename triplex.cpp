d35522cd8783cc03c5739eaea98c385e9684bb5b
Julien Dendauw <dauwbot@pm.me>
2021-05-14 16:44:01 +0200

    Some comments to follow along

--- triplex.cpp
+++ triplex.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 
 int main() {
+    // General description
     std::cout << "You're the dumbest person on earth and need to enter the correct code to collect your booze";
     std::cout << std::endl;
     std::cout << "Enter the correct code to access the tavern...";
     std::cout << std::endl;
 
+    /* Assigning variables */
     const int a = 4;
     int b = 3;
     int c = 2;
@@ -13,6 +15,7 @@ int main() {
     int sum = a + b + c;
     int prod = a * b * c;
 
+    // Print sum and product
     std::cout << sum << std::endl;
     std::cout << prod;
 
